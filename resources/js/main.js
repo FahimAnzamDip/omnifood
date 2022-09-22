66087a12002b750bbd471f8610af428ef511d760
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-09-22 18:40:07 +0600

    back top top button added

--- resources/js/main.js
+++ resources/js/main.js
@@ -126,3 +126,31 @@ $(document).ready(function () {
     gallery: { enabled: true }
   });
 });
+
+
+// back to top
+window.addEventListener("scroll", function () {
+  var nav = this.document.querySelector("nav");
+  nav.classList.toggle("sticky", this.window.scrollY > 20);
+});
+
+let myBtn = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    myBtn.style.display = 'block';
+  } else {
+    myBtn.style.display = 'none';
+  }
+}
+
+myBtn.addEventListener('click', backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
