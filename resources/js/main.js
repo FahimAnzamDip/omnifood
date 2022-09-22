500dfa51d37b92c73656b4feb771c0a600d83589
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-09-22 19:13:11 +0600

    slider updated

--- resources/js/main.js
+++ resources/js/main.js
@@ -128,7 +128,32 @@ $(document).ready(function () {
   $('.feedback').slick({
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
 
